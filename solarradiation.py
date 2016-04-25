2dba012108d2b437be7483aa741e7fadb0a1c701
mmanana <mananam@unican.es>
2016-04-25 18:33:06 +0200

    solar radiation
    solarradiation.py .- source code of a function devoted to the
    computation of the density of solar radiation as a function of the day
    of the year.
    solarradiation_example.ipynb example of use of the function
    solarradiation.py

---
+++ solarradiation.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 18:20:11 2016
+
+File: solarradiation.py
+Description: Compute solar radiation
+
+Input:
+n: Day of the year. Integer [1, 365]
+
+Output:
+Gon: Solar radiation density
+
+@author: Mario
+"""
+import math
+
+def Gon( n):
+    GSC=1367.0 # W/m^2     
+    Gon=GSC*(1+0.033*math.cos(2.0*math.pi*n/365.0))
+    return Gon
